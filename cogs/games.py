b41398248b505fa76ee4782ba2cdd34e0e8d5a2f
sushipro-314 <jevalent2000@hotmail.com>
2025-06-23 15:40:23 -0700

    added role leveling

--- cogs/games.py
+++ cogs/games.py
@@ -99,12 +99,12 @@ class Games(commands.Cog):
         pass
     
     @commands.hybrid_group(help="View rank information.")
-    async def rank(self, ctx, member=None):
-        member_obj = (ctx.message.author or member)
+    async def rank(self, ctx):
+        member_obj = (ctx.message.author)
         json_data = await self.fetch_user_levels(member=member_obj)
         rank_embed = discord.Embed(
                 title=f"Ranking information for **{[member_obj.name if member_obj else 'Unknown'][0]}**",
-                description=f"User <@{json_data['uid']}> is currently level **{str(json_data['rank'])}**"
+                description=f"User <@{json_data['uid']}> is currently level **{str(json_data['rank'])}**. This member has **{str(json_data['xp'])}** XP!"
             )
         await ctx.send(embeds=[rank_embed])
     
@@ -183,7 +183,75 @@ class Games(commands.Cog):
             ])
         else:
             logging.error("Failed to set rank channel: guild data not found")
+            await ctx.send("Guild data was not found. Bot may have to rejoin the server!")
+    
+    @rankconfig.command(help="Adds a role for when a user levels up.")
+    @commands.has_permissions(manage_guild=True)
+    async def addrole(self, ctx, role: discord.Role, amount_until=100):
+        guild_data = await data_parser.get_guild_data(ctx.guild.id)
+        if guild_data is not None:
+            if guild_data.get("rank_roles") is None:
+                guild_data["rank_roles"] = [{"id": role.id, "amount": amount_until }]
+            else:
+                guild_data["rank_roles"].append({"id": role.id, "amount": amount_until})
+            await data_parser.write_guild_data(ctx.guild.id, data=guild_data)
+            await ctx.send("Added role data to ranking roles! Users can now get this role when they level up!")
+        else:
+            logging.error("Failed to set rank channel: guild data not found")
             await ctx.send("Guild data was not found.")
+
+    @rankconfig.command(help="Lists all roles for leveling up users.")
+    @commands.has_permissions(manage_guild=True)
+    async def viewroles(self, ctx):
+        guild_data = await data_parser.get_guild_data(ctx.guild.id)
+        if guild_data is not None:
+            rank_role_strings = []
+            if guild_data.get("rank_roles") is None:
+                guild_data["rank_roles"] = []
+                await data_parser.write_guild_data(ctx.guild.id, data=guild_data)
+            else:
+                for role_data in guild_data["rank_roles"]:
+                    if role_data.get("id") and role_data.get("amount"):
+                        logging.info("Removing role data for matching role")
+                        rank_role_strings.append(discord.Embed(
+                            title=str(role_data.get("id")),
+                            description=f"Users can obtain this role at level {str(role_data.get('amount'))}!"
+                        ))
+                    else:
+                        logging.info("Skipping role because it does not match.")
+                await ctx.send("The following roles are for leveling up!", embeds=rank_role_strings)
+        else:
+            logging.error("Failed to set rank channel: guild data not found")
+            await ctx.send("Guild data was not found.")
+
+    @rankconfig.command(help="Removes a role for when a user levels up.")
+    @commands.has_permissions(manage_guild=True)
+    async def remrole(self, ctx, role: discord.Role):
+        guild_data = await data_parser.get_guild_data(ctx.guild.id)
+        if guild_data is not None:
+            if guild_data.get("rank_roles") is None:
+                guild_data["rank_roles"] = []
+            else:
+                for role_data in guild_data["rank_roles"]:
+                    if role_data.get("id") and (role_data.get("id") == role.id):
+                        logging.info("Removing role data for matching role")
+                        guild_data["rank_roles"].remove(role_data)
+                    else:
+                        logging.info("Skipping role because it does not match.")
+            await data_parser.write_guild_data(ctx.guild.id, data=guild_data)
+            await ctx.send("Removed role data to ranking roles! Users can no longer get this role when they level up!")
+        else:
+            logging.error("Failed to set rank channel: guild data not found")
+            await ctx.send("Guild data was not found.")
+
+    async def level_up_user(self, json_data, guild_data, member_obj):
+        json_data['xp'] = 0
+        json_data['rank'] += 1
+        if guild_data and guild_data.get("rank_channel"):
+            await (await member_obj.guild.fetch_channel(guild_data["rank_channel"])).send(embeds=[discord.Embed(
+                title=f"{member_obj.display_name} has leveled up to level " + str(json_data["rank"]),
+                description=f"User <@{json_data['uid']}> is currently level **{json_data['rank']}** now! Congratulations!"
+            )])
 
     @commands.Cog.listener()
     @commands.guild_only()
@@ -194,14 +262,15 @@ class Games(commands.Cog):
         guild_data = await data_parser.get_guild_data(guild_id=member_obj.guild.id)
         if (await self.vaildate_json(json_data, member_obj, guild_data)):
             json_data['xp'] += guild_data['xp_gain']
-            if json_data['xp'] >= guild_data["xp_cost"]:
-                json_data['xp'] = 0
-                json_data['rank'] += 1
-                if guild_data and guild_data.get("rank_channel"):
-                    await (await member_obj.guild.fetch_channel(guild_data["rank_channel"])).send(embeds=[discord.Embed(
-                        title=f"{member_obj.display_name} has leveled up to level " + str(json_data["rank"]),
-                        description=f"User <@{json_data['uid']}> is currently level **{json_data['rank']}** now! Congratulations!"
-                )])
+            if (guild_data.get("rank_roles") is not None) and len(guild_data["rank_roles"]) > 0:
+                for role in guild_data["rank_roles"]:
+                    if json_data["rank"] >= role["amount"]:
+                        try:
+                            await self.bot.add_roles(message.guild.get_role(role["id"]))
+                        except Exception:
+                            logging.warn("Unable to add role to user even though user has correct levels. Be sure the bot has proper permissions.")
+            if (json_data['xp'] >= guild_data["xp_cost"]):
+                await self.level_up_user(json_data=json_data, guild_data=guild_data, member_obj=member_obj)
             await data_parser.write_member_data(member=member_obj, data=json_data)
         else:
             logging.error("Could not validate leveling info for member... Member ID: " + str(json_data['uid']) + "... Displaying error information")
